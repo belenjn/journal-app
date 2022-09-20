8dd6a7a2edd3a47a69c1633460adc8fc9d1c79a5
Belen JN <belenjarabanavas31@gmail.com>
2022-09-20 10:31:09 +0200

    Solving erros in routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
+import {JournalRoutes} from "../journal/routes/JournalRoutes";
 
 export const AppRouter = () => {
   return (
