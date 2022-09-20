01f9066cff0709c3e1acafc56e35e45b8c301c4c
Belen JN <belenjarabanavas31@gmail.com>
2022-09-20 11:13:43 +0200

    Styiling Login Page

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import { Grid, TextField, Typography } from "@mui/material";
+import React from "react";
 
 export const LoginPage = () => {
   return (
-    <div>
-        Login Page
-    </div>
-  )
-}
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{
+        minHeight: "100vh",
+        backgroundColor: "primary.main",
+        padding: 4,
+      }}
+    >
+      <Grid
+        item
+        className="box-shadow"
+        xs={3}
+        sx={{
+          backgroundColor: "white",
+          padding: 3,
+          borderRadius: 2,
+        }}
+      >
+        <Typography
+          variant="h5"
+          sx={{
+            marginBottom: 1,
+          }}
+        >
+          Login
+        </Typography>
+
+        <form>
+          <Grid container>
+            <Grid item>
+              <TextField
+                label="Correo"
+                type="email"
+                placeholder="correo@google.com"
+                fullWidth
+              />
+            </Grid>
+
+            <Grid item>
+              <TextField
+                label="Contraseña"
+                type="password"
+                placeholder="Contraseña"
+                fullWidth
+              />
+            </Grid>
+          </Grid>
+        </form>
+      </Grid>
+    </Grid>
+  );
+};
