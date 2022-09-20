1bebe24e4cf9a3045325cc88c82bf23d368eb207
Belen JN <belenjarabanavas31@gmail.com>
2022-09-20 10:18:20 +0200

    New folders

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+        
+      /* login and reg */
+      <Route />
+
+
+      /* journal app */
+      <Route />
+
+    </Routes>
+  );
+};
