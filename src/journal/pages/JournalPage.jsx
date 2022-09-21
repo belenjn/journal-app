c71f872ec106696584f248e3de0beca6450cf0c5
Belen JN <belenjarabanavas31@gmail.com>
2022-09-21 11:32:16 +0200

    NoteView created

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { JournalLayout } from "../layouts/JournalLayout";
+import { NoteView } from "../views/NoteView";
 import { NothingSelectedView } from "../views/NothingSelectedView";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <NothingSelectedView/>
+      {/* <NothingSelectedView/> */}
+      <NoteView/>
     </JournalLayout>
   );
 };
