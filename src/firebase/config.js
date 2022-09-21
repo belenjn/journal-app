ae0a605cfc4ebec383720f9b39630183c82ab160
Belen JN <belenjarabanavas31@gmail.com>
2022-09-21 16:08:52 +0200

    Env.js for env

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { firebaseEnv } from "../env";
+
+const firebaseConfig = {
+  apiKey: firebaseEnv.apiKey,
+  authDomain: firebaseEnv.authDomain,
+  projectId: firebaseEnv.projectId,
+  storageBucket: firebaseEnv.storageBucket,
+  messagingSenderId: firebaseEnv.messagingSenderId,
+  appId: firebaseEnv.appId,
+};
+
+const app = initializeApp(firebaseConfig);
