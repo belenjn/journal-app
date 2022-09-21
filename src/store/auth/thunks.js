05535195e89817cf9bba6d76a678fccd46a8657c
Belen JN <belenjarabanavas31@gmail.com>
2022-09-21 16:26:55 +0200

    providers config

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,13 +1,16 @@
-import { checkingCredentials } from "./authSlice"
+import { signInWithGoogle } from "../../firebase/providers";
+import { checkingCredentials } from "./authSlice";
 
 export const checkingAuthentication = (email, passowrd) => {
-    return async(dispatch) => {
-        dispatch(checkingCredentials());
-    }
-}
+  return async (dispatch) => {
+    dispatch(checkingCredentials());
+  };
+};
 
 export const startGoogleSignIn = () => {
-    return async(dispatch) => {
-        dispatch(checkingCredentials());
-    }
-}
+  return async (dispatch) => {
+    dispatch(checkingCredentials());
+    const result = await signInWithGoogle();
+    console.log({result})
+  };
+};
