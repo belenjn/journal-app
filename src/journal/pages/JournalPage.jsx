c6971937ad9664ed04714b8bbaa7a159a953d141
Belen JN <belenjarabanavas31@gmail.com>
2022-09-21 11:17:35 +0200

    NothingSelectedView created

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,11 @@
-import { Typography } from "@mui/material";
 import React from "react";
 import { JournalLayout } from "../layouts/JournalLayout";
+import { NothingSelectedView } from "../views/NothingSelectedView";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
-        Dolor sunt sunt incididunt do amet consectetur nulla incididunt enim voluptate. Deserunt velit exercitation minim excepteur amet incididunt aliqua. Occaecat irure amet sit deserunt cillum sit ex. Lorem magna nostrud enim deserunt proident in. Laborum nulla officia deserunt commodo eu.
-      </Typography>
+      <NothingSelectedView/>
     </JournalLayout>
   );
 };
