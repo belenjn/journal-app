8345f1dd8118c544d54f64e5cc4a152a1fcca819
Belen JN <belenjarabanavas31@gmail.com>
2022-09-22 16:28:41 +0200

    Spinner created

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
 import {JournalRoutes} from "../journal/routes/JournalRoutes";
+import { CheckingAuth } from "../ui/components/CheckingAuth";
 
 export const AppRouter = () => {
+
+
+  const {status} = useSelector(state => state.auth);
+
+  if(status === 'checking') {
+    return <CheckingAuth/>
+  }
+
   return (
     <Routes>
         
