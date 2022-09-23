5eb2df220550c75df90d5abcf5f1ce0cfadf5892
Belen JN <belenjarabanavas31@gmail.com>
2022-09-23 10:52:57 +0200

    Slice added in store

--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -4,7 +4,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const journalSlice = createSlice({
     name: 'journal',
     initialState: {
-
+        isSaving: true
     },
     reducers: {}
 })
