38f5cf28056324da33c030e3828e79223dfb6a49
Belen JN <belenjarabanavas31@gmail.com>
2022-09-23 10:52:15 +0200

    journalSlice created

---
+++ src/store/journal/journalSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+export const journalSlice = createSlice({
+    name: 'journal',
+    initialState: {
+
+    },
+    reducers: {}
+})
+
+export const {} = journalSlice.actions;
