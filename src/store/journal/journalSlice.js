e5bd941eaff4e556d69b0c198c39e23ac37ca261
Belen JN <belenjarabanavas31@gmail.com>
2022-09-23 10:57:38 +0200

    Reducers created in journalSlice

--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -1,12 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
 export const journalSlice = createSlice({
-    name: 'journal',
-    initialState: {
-        isSaving: true
-    },
-    reducers: {}
-})
+  name: "journal",
+  initialState: {
+    isSaving: true,
+    messageSaved: "",
+    notes: [],
+    active: null,
+  },
+  reducers: {
+    addNewEmptyNote: (state, action) => {},
+    setActiveNote: (state, action) => {},
+    setNotes: (state, action) => {},
+    setSaving: (state) => {},
+    updateNote: (state, action) => {},
+    deleteNoteById: (state, action) => {},
+  },
+});
 
 export const {} = journalSlice.actions;
