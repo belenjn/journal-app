05e37b8ad263c265fe8db0b4279e7a90910e382d
Belen JN <belenjarabanavas31@gmail.com>
2022-10-04 10:59:01 +0200

    First test with thunks and new config: jest.config

---
+++ tests/store/auth/thunks.test.js
@@ -0,0 +1,16 @@
+import { checkingCredentials } from "../../../src/store/auth/authSlice";
+import { checkingAuthentication } from "../../../src/store/auth/thunks";
+
+jest.mock("../../../src/firebase/providers");
+
+describe("Pruebas en authThunks", () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => jest.clearAllMocks());
+
+  test("debe de invocar el checkingCredentials", async () => {
+    await checkingAuthentication()(dispatch);
+
+    expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+  });
+});
